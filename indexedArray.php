52515c9a374f941a679cd343224b574caec8732d
MohdFazly <2020819344@student.uitm.edu.my>
2022-05-12 22:17:28 +0800

    dd

--- indexedArray.php
+++ indexedArray.php
@@ -10,6 +10,7 @@
     <?php
     $month = array ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
     'October', 'November', 'December');
+    print_r($array_name);
 
     $i = 0;
     for($i = 0; $i <= 11; $i++)
