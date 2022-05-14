26df1bb4f534f6475cd92f2a319fbdbd57454bb2
MohdFazly <2020819344@student.uitm.edu.my>
2022-05-14 10:00:29 +0800

    fff

--- assocArray.php
+++ assocArray.php
@@ -14,9 +14,18 @@
     'July' => 31, 'August' => 31,
     'September' => 30, 'October' => 31,
     'November' => 30, 'December' => 31);
-    echo $monthDays['January']<br>;
-    echo $monthDays['February']<br>;
-    echo $monthDays['March']<br>;
+    echo $monthDays['January']"<br>";
+    echo $monthDays['February']"<br>";
+    echo $monthDays['March']"<br>";
+    echo $monthDays['April']"<br>";
+    echo $monthDays['May']"<br>";
+    echo $monthDays['June']"<br>";
+    echo $monthDays['July']"<br>";
+    echo $monthDays['August']"<br>";
+    echo $monthDays['September']"<br>";
+    echo $monthDays['October']"<br>";
+    echo $monthDays['November']"<br>";
+    echo $monthDays['December']"<br>";
     
     ?>
 
