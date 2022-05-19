fc2d11ea2b547efa612f48a2da2608f8fddbf398
MohdFazly <2020819344@student.uitm.edu.my>
2022-05-19 23:55:23 +0800

    last commit

--- exercise.php
+++ exercise.php
@@ -9,7 +9,7 @@
 <body>
     
     <?php
-    $monthDays = array ('Splorch' => 23, 'Sploo' => 28, 'Splat' => v2, 'Splatt' => 3, 'Spleen' => 44, 'Splune' => 30, 'Spling' => 61, 'Slendo' => 61,
+    $monthDays = array ('Splorch' => 23, 'Sploo' => 28, 'Splat' => 'v2', 'Splatt' => 3, 'Spleen' => 44, 'Splune' => 30, 'Spling' => 61, 'Slendo' => 61,
     'Sploctember' => 31, 'Splictember' => 31, 'Splanet' => 30, 'TheRest' => 22);
     ?>
 
