1909cccdeac2d6819e5e47ca2148c24e01b9f120
MohdFazly <2020819344@student.uitm.edu.my>
2022-05-19 23:36:47 +0800

    ggg

--- assocArray.php
+++ assocArray.php
@@ -23,23 +23,16 @@
     ?>
     
     <?php
-    $monthDays = range(4,30);
-    print_r ($monthDays); 
+   foreach($monthDays as $key => $day30)
+   {
+       if($day30 == 30)
+       {
+           echo "$key $day30 <br>";
+       }
+   }
     ?>
     
-    <?php
-    function assign_key() { 
-    return 'd'; 
-    } 
-    $month['January'] = 1; 
-    $month['b'] = 2; 
-    $month['c'] = 3; 
-    $month[assign_key()] = 4;
-    $month['b'] = $month['a'] + $month['c'];
-   
-    ?>
-
-
+    
 
 </body>
 </html>
